c4a3c39811b5077e718e5fc82f53be4f60589f45
osamaashraf6 <osamaashraf668@gmail.com>
2022-11-22 18:56:38 +0200

    Pull the channges from the dev branch

--- src/index.js
+++ src/index.js
@@ -1,13 +1,55 @@
-import _ from 'lodash';
-import './style.css';
+// start import
+import "./style.css";
+const footer = require("./modules/footer.js");
+// start import
 
-function component() {
-  const element = document.createElement('div');
+// start all required elements
+const parfoot = document.querySelector(".footer__container__parfoot");
+// end all required elements
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
+// start footer UI
+const footerUI = (items) => {
+  items.forEach(() => {
+    parfoot.innerHTML = `
+    <div class="footer__container__parfoot__foot">
+            <h2>${items[0]}</h2>
+            <p>${items[1]}</p>
+            <h3>${items[2]}</h3>
+            <p>${items[3]}</p>
+          </div>
+          <div class="footer__container__parfoot__foot">
+            <h3>${items[4]}</h3>
+            <ul>
+              <li><a href="">${items[5]}</a></li>
+              <li><a href="">${items[6]}</a></li>
+              <li><a href="">${items[7]}</a></li>
+              <li><a href="">${items[8]}</a></li>
+              <li><a href="">${items[9]}</a></li>
+            </ul>
+          </div>
+          <div class="footer__container__parfoot__foot">
+            <h3>${items[10]}</h3>
+            <ul>
+              <li><a href="">${items[11]}</a></li>
+              <li><a href="">${items[12]}</a></li>
+              <li><a href="">${items[13]}</a></li>
+              <li><a href="">${items[14]}</a></li>
+              <li><a href="">${items[15]}</a></li>
+            </ul>
+          </div>
+          <div class="footer__container__parfoot__foot">
+            <h3>${items[16]}</h3>
+            <ul>
+              <li><a href="">${items[17]}</a></li>
+              <li><a href="">${items[18]}</a></li>
+              <li><a href="">${items[19]}</a></li>
+             <div class="formcontrol">
+              <input type="email" name="email" placeholder="Email">
+             </div>
+            </ul>
+          </div>
+    `;
+  });
+};
+footerUI(footer);
+// end UI footer UI
