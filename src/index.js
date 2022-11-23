94e0d446db0ffdfe1a7f254c290078f80dd58afb
osamaashraf6 <osamaashraf668@gmail.com>
2022-11-23 15:54:26 +0200

    Add the js file for display the data of the footer

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-import _ from 'lodash';
+// start import
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const footer = require('./modules/footer.js');
+// start import
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+// start all required elements
+const last = document.querySelector('footer .last');
+// end all required elements
 
-  return element;
-}
-
-document.body.appendChild(component());
+// start footer UI
+const footerUI = (items) => {
+  items.forEach(() => {
+    last.innerHTML = `
+    <div class="">${items[0]}</div>
+    <div class="">${items[1]}</div>
+          <div class="">${items[2]}</div>
+    `;
+  });
+};
+footerUI(footer);
+// end UI footer UI
