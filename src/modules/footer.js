5ad30e2dfc26fa8b9e838c2567030fcf683f3952
osamaashraf6 <osamaashraf668@gmail.com>
2022-11-23 17:08:28 +0200

    Try to fix the linters errors

--- src/modules/footer.js
+++ src/modules/footer.js
@@ -1,7 +1,6 @@
 const footer = [
-    '©2020 Charry. All rights reserved.',
-    'powered By : Osama And Nela',
-    'Privacy Policy | Terms & Conditions',
-  ];
-  module.exports = footer;
-  
+  '©2020 Charry. All rights reserved.',
+  'powered By : Osama And Nela',
+  'Privacy Policy | Terms & Conditions',
+];
+module.exports = footer;
