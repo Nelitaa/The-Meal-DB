7aa7f1c9d65f41809bd11ee81e292b9d08d773b6
osamaashraf6 <osamaashraf668@gmail.com>
2022-11-24 20:51:26 +0200

    Add the comments file for post the data in the API

---
+++ src/modules/postcomments.js
@@ -0,0 +1,15 @@
+const postComments = async (data) => {
+  fetch(
+    `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/dJkBaFu7e8jdpHbmcAqU/comments?item_id=${data.item_id}`,
+    {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        item_id: data.item_id,
+        username: data.username,
+        comment: data.comment,
+      }),
+    },
+  );
+};
+export default postComments;
