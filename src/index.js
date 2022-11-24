0213d48de80a0d023a2b84c14b7ec66ce9c90784
osamaashraf6 <osamaashraf668@gmail.com>
2022-11-24 17:33:20 +0200

    Fix the linters errors

--- src/index.js
+++ src/index.js
@@ -1,24 +1,22 @@
-import "./style.css";
-import Meal from "./modules/meal_class.js";
-import Api from "./modules/api_class.js";
-import RENDER from "./modules/render.js";
-import { name } from "./modules/apicomments.js";
-console.log(name);
+import './style.css';
+import Meal from './modules/meal_class.js';
+import Api from './modules/api_class.js';
+import RENDER from './modules/render.js';
 
-window.addEventListener("load", () => {
+window.addEventListener('load', () => {
   const API = new Api();
   API.getMeals().then((data) => {
     const all = data.meals;
     const MEALS = data.meals.map(
-      (meal) => new Meal(meal.strMeal, meal.strMealThumb, meal.idMeal)
+      (meal) => new Meal(meal.strMeal, meal.strMealThumb, meal.idMeal),
     );
     RENDER(MEALS, all);
   });
 });
 
-const parmodal = document.querySelector(".parmodal");
-window.addEventListener("click", (e) => {
-  if (e.target == parmodal) {
-    parmodal.style.display = "none";
+const parmodal = document.querySelector('.parmodal');
+window.addEventListener('click', (e) => {
+  if (e.target === parmodal) {
+    parmodal.style.display = 'none';
   }
 });
--- src/modules/render.js
+++ src/modules/render.js
@@ -1,53 +1,50 @@
-const close = () => {
-  console.log("click");
-};
-const ALL_MEALS = document.querySelector("#all-meals");
+const ALL_MEALS = document.querySelector('#all-meals');
 const RENDER = (meals, alls) => {
   meals.forEach((meal) => {
-    const MEAL = document.createElement("div");
-    MEAL.className = "meal";
+    const MEAL = document.createElement('div');
+    MEAL.className = 'meal';
     ALL_MEALS.appendChild(MEAL);
 
-    const MEAL_IMAGE = document.createElement("img");
-    MEAL_IMAGE.className = "meal-image";
+    const MEAL_IMAGE = document.createElement('img');
+    MEAL_IMAGE.className = 'meal-image';
     MEAL_IMAGE.src = meal.image;
     MEAL_IMAGE.alt = meal.name;
     MEAL.appendChild(MEAL_IMAGE);
 
-    const CONTAINER_NAME_LIKES = document.createElement("div");
-    CONTAINER_NAME_LIKES.className = "container-name-likes";
+    const CONTAINER_NAME_LIKES = document.createElement('div');
+    CONTAINER_NAME_LIKES.className = 'container-name-likes';
     MEAL.appendChild(CONTAINER_NAME_LIKES);
 
-    const MEAL_NAME = document.createElement("h2");
-    MEAL_NAME.className = "meal-name";
+    const MEAL_NAME = document.createElement('h2');
+    MEAL_NAME.className = 'meal-name';
     MEAL_NAME.innerHTML = meal.name;
     CONTAINER_NAME_LIKES.appendChild(MEAL_NAME);
 
-    const CONTAINER_LIKES = document.createElement("div");
-    CONTAINER_LIKES.className = "container-likes";
+    const CONTAINER_LIKES = document.createElement('div');
+    CONTAINER_LIKES.className = 'container-likes';
     CONTAINER_NAME_LIKES.appendChild(CONTAINER_LIKES);
 
-    const LIKE_IMG = document.createElement("img");
-    LIKE_IMG.className = "like-icon";
-    LIKE_IMG.src = "./assets/heart-line.png";
-    LIKE_IMG.alt = "like icon";
+    const LIKE_IMG = document.createElement('img');
+    LIKE_IMG.className = 'like-icon';
+    LIKE_IMG.src = './assets/heart-line.png';
+    LIKE_IMG.alt = 'like icon';
     CONTAINER_LIKES.appendChild(LIKE_IMG);
 
-    const LIKES = document.createElement("p");
-    LIKES.className = "likes";
-    LIKES.innerHTML = "5 likes";
+    const LIKES = document.createElement('p');
+    LIKES.className = 'likes';
+    LIKES.innerHTML = '5 likes';
     CONTAINER_LIKES.appendChild(LIKES);
 
-    const COMMENTS = document.createElement("button");
-    COMMENTS.className = "comments";
-    COMMENTS.innerHTML = "Comments";
-    COMMENTS.setAttribute("name", `${meal.id}`);
-    COMMENTS.addEventListener("click", (e) => {
+    const COMMENTS = document.createElement('button');
+    COMMENTS.className = 'comments';
+    COMMENTS.innerHTML = 'Comments';
+    COMMENTS.setAttribute('name', `${meal.id}`);
+    COMMENTS.addEventListener('click', (e) => {
       const a = e.target.name;
-      const res = alls.filter((item) => item.idMeal == a);
+      const res = alls.filter((item) => item.idMeal === a);
       // console.log(res[0]);
-      const parmodal = document.querySelector(".parmodal");
-      parmodal.style.display = "grid";
+      const parmodal = document.querySelector('.parmodal');
+      parmodal.style.display = 'grid';
       parmodal.innerHTML = `
       <div class="parmodal__modal">
         <div class="front"><button id="close" class="close">&times;</button></div>
@@ -90,15 +87,15 @@ const RENDER = (meals, alls) => {
         </div>
       </div>
     `;
-      const close = document.querySelector("#close");
-      close.addEventListener("click", () => {
-        parmodal.style.display = "none";
+      const close = document.querySelector('#close');
+      close.addEventListener('click', () => {
+        parmodal.style.display = 'none';
       });
     });
     MEAL.appendChild(COMMENTS);
-    const RESERVATIONS = document.createElement("button");
-    RESERVATIONS.className = "reservations";
-    RESERVATIONS.innerHTML = "Reservations";
+    const RESERVATIONS = document.createElement('button');
+    RESERVATIONS.className = 'reservations';
+    RESERVATIONS.innerHTML = 'Reservations';
     MEAL.appendChild(RESERVATIONS);
   });
 };
