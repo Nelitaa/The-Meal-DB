7428f3ba665dafbc49429124956b5dd6c64a7857
Marianela Muñoz Gutierrez <marianelamunozgutierrez@gmail.com>
2022-11-24 15:06:26 -0400

    Add meals_counter.test.js

--- src/modules/meals_counter.js
+++ src/modules/meals_counter.js
@@ -1,9 +1,19 @@
 const mealsCounter = () => {
   const mealCounter = document.querySelectorAll('.meal-counter');
+  const totalMeals = document.querySelectorAll('.total-meals');
   let counter = 1;
+
   mealCounter.forEach((meal) => {
     meal.innerHTML = `Meal ${counter}`;
     counter += 1;
+  });
+
+  totalMeals.forEach((total) => {
+    if(mealCounter.length < 1){
+      total.innerHTML = ` (${0})`;
+    }else {
+      total.innerHTML = ` (${counter - 1})`;
+    }
   });
 };
 
